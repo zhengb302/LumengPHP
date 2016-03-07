461b1660c6eb70a2f37184527b3b381292f3973d
zhengluming <908235332@qq.com>
2016-03-07 23:48:17 +0800

    新增测试：testPathWithPlaceholder

---
+++ tests/Commands/ShowOrderCommand.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace tests\Commands;
+
+use LumengPHP\Kernel\AppContext;
+use LumengPHP\Command\Command;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+/**
+ * 显示订单明细命令
+ *
+ * @author Lumeng <zhengb302@163.com>
+ */
+class ShowOrderCommand implements Command {
+
+    public function init(AppContext $appContext) {
+        
+    }
+
+    public function execute(Request $request) {
+        $id = $request->attributes->get('id');
+        $name = $request->query->get('name');
+        return new Response("id is {$id}, name is {$name}");
+    }
+
+}
--- tests/TestCases/FullWorkflowTest.php
+++ tests/TestCases/FullWorkflowTest.php
@@ -25,4 +25,17 @@ class FullWorkflowTest extends \PHPUnit_Framework_TestCase {
         $kernel->terminate($request, $response);
     }
 
+    public function testPathWithPlaceholder() {
+        $request = Request::create('/order/showOrder/38/?name=lilei', 'GET');
+
+        $kernel = new AppKernel(TEST_ROOT . '/config/config.php');
+
+        $response = $kernel->handle($request);
+
+        $this->expectOutputString('id is 38, name is lilei');
+        $response->send();
+
+        $kernel->terminate($request, $response);
+    }
+
 }

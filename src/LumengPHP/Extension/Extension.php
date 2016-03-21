a6aa3d157cf46fb49d20033c227432f0c5554599
zhengluming <908235332@qq.com>
2016-03-21 21:58:16 +0800

    扩展机制

--- src/LumengPHP/Extension/Extension.php
+++ src/LumengPHP/Extension/Extension.php
@@ -6,7 +6,7 @@ use LumengPHP\Kernel\AppContext;
 use LumengPHP\DependencyInjection\ServiceContainer;
 
 /**
- * 扩展接口，用以优雅的集成组件。<br />
+ * 扩展接口，用以优雅的集成组件、扩展功能。<br />
  * 在扩展里边，可以配置组件、注册服务、做一些其他事情等。
  * 一个组件，如果配置起来比较简单，则可以作为一个服务；如果配置起来比较复杂，
  * 像database、logger、cache等，则可以作为扩展来加载。

a6086238d1308eae2c4c5597690311a8ea3234b8
zhengluming <908235332@qq.com>
2016-04-10 15:00:38 +0800

    配置优化；命令行工具

--- src/LumengPHP/Console/Application.php
+++ src/LumengPHP/Console/Application.php
@@ -3,6 +3,10 @@
 namespace LumengPHP\Console;
 
 use Symfony\Component\Console\Application as SymfonyApplication;
+use LumengPHP\Kernel\AppContext;
+use LumengPHP\Kernel\AppContextImpl;
+use LumengPHP\Kernel\AppConfig;
+use LumengPHP\DependencyInjection\ServiceContainer;
 
 /**
  * 命令行应用
@@ -10,5 +14,26 @@ use Symfony\Component\Console\Application as SymfonyApplication;
  * @author Lumeng <zhengb302@163.com>
  */
 class Application extends SymfonyApplication {
-    
+
+    /**
+     * @var AppContext AppContext实例
+     */
+    private $appContext;
+
+    public function __construct($configFilePath) {
+        parent::__construct();
+
+        $appConfig = new AppConfig(require($configFilePath));
+        $container = new ServiceContainer(array());
+        $this->appContext = new AppContextImpl($appConfig, $container);
+    }
+
+    /**
+     * 返回AppContext实例
+     * @return AppContext
+     */
+    public function getAppContext() {
+        return $this->appContext;
+    }
+
 }

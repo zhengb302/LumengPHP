d7877e5478891442191ea6922ec7246559142e93
zhengluming <908235332@qq.com>
2017-08-15 21:24:43 +0800

    新架构

--- tests/Services/Bar.php
+++ tests/Services/Bar.php
@@ -3,14 +3,10 @@
 namespace tests\Services;
 
 /**
- * Foo服务
+ * Bar服务
  *
  * @author zhengluming <luming.zheng@shandjj.com>
  */
-class Foo {
-
-    public function foo() {
-        return 'foo!';
-    }
-
+class Bar {
+    
 }
--- tests/TestCases/ServiceContainerTest.php
+++ tests/TestCases/ServiceContainerTest.php
@@ -25,4 +25,42 @@ class ServiceContainerTest extends PHPUnit_Framework_TestCase {
         $this->assertEquals('foo!', $foo->foo());
     }
 
+    public function testServiceNotExists() {
+        $configs = [
+            'foo' => [
+                'class' => \tests\Services\Foo::class,
+            ],
+        ];
+        $container = new ServiceContainer($configs);
+
+        $bar = $container->get('bar');
+        $this->assertNull($bar);
+    }
+
+    public function testRegService() {
+        $container = new ServiceContainer([]);
+        $container->register('foo', new \tests\Services\Foo());
+
+        $this->assertTrue($container->has('foo'));
+
+        $foo = $container->get('foo');
+        $this->assertInstanceOf(\tests\Services\Foo::class, $foo);
+    }
+
+    public function testCallback() {
+        $configs = [
+            'foo' => function($container) {
+                return new \tests\Services\Foo();
+            },
+        ];
+        $container = new ServiceContainer($configs);
+
+        $foo = $container->get('foo');
+        $this->assertInstanceOf(\tests\Services\Foo::class, $foo);
+    }
+
+    public function testArguments() {
+        
+    }
+
 }

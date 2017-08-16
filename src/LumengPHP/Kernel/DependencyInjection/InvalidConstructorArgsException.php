9089ba42cef89446c4510bbdbb58c476a7de4005
zhengluming <908235332@qq.com>
2017-08-16 16:23:26 +0800

    新架构

---
+++ src/LumengPHP/Kernel/DependencyInjection/InvalidConstructorArgsException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace LumengPHP\Kernel\DependencyInjection;
+
+use Exception;
+
+/**
+ * 无效的服务构造器参数
+ *
+ * @author zhengluming <luming.zheng@shandjj.com>
+ */
+class InvalidConstructorArgsException extends Exception {
+    
+}

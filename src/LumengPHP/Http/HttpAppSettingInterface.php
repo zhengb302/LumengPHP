30423d4364cb484b4f66e4dce47f61226cc5a971
zhengluming <908235332@qq.com>
2017-09-26 12:42:50 +0800

    拦截器链

--- src/LumengPHP/Http/HttpAppSettingInterface.php
+++ src/LumengPHP/Http/HttpAppSettingInterface.php
@@ -13,6 +13,15 @@ interface HttpAppSettingInterface extends AppSettingInterface {
 
     /**
      * 取得拦截器配置
+     * 
+     * 格式：拦截器类全限定名称 => 拦截模式
+     * 示例：
+     * [
+     *     Profiler::class => '*',
+     *     LogFlusher::class => '*',
+     *     UserAuth::class => '*, ~/user/login, ~/other/help',
+     * ]
+     * 
      * @return array
      */
     public function getInterceptors();
@@ -21,6 +30,7 @@ interface HttpAppSettingInterface extends AppSettingInterface {
      * 取得路由配置<br />
      * 应用根据使用的路由实现，返回相应的路由配置：可以是一个数组，也可以是一个字符串、对象实例，
      * 或者如果不需要路由配置，直接返回null
+     * 
      * @return mixed 
      */
     public function getRoutingConfig();

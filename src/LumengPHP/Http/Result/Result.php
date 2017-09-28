7cda3461a59490bcf345e2bb99e7554e68ab6ae0
zhengluming <908235332@qq.com>
2017-09-28 16:56:57 +0800

    Result对象增加more属性

--- src/LumengPHP/Http/Result/Result.php
+++ src/LumengPHP/Http/Result/Result.php
@@ -20,14 +20,20 @@ class Result {
     private $msg;
 
     /**
-     * @var array (携带的)数据
+     * @var array (携带的)数据。这个数据是比较正式的数据。
      */
     private $data;
+
+    /**
+     * @var array 更多数据。用于携带除了正式数据之外的数据，如调试信息等。
+     */
+    private $more;
 
     public function __construct($status, $msg = '', array $data = []) {
         $this->status = $status;
         $this->msg = $msg;
         $this->data = $data;
+        $this->more = [];
     }
 
     public function getStatus() {
@@ -42,6 +48,10 @@ class Result {
         return $this->data;
     }
 
+    public function getMore() {
+        return $this->more;
+    }
+
     public function setStatus($status) {
         $this->status = $status;
     }
@@ -54,12 +64,26 @@ class Result {
         $this->data = $data;
     }
 
+    public function setMore(array $more) {
+        $this->more = $more;
+    }
+
+    /**
+     * 返回字符串形式的Result对象，子类可以覆盖此方法用以定制特定的字符串格式。
+     * 
+     * @return string
+     */
     public function __toString() {
-        return json_encode([
+        $result = [
             'status' => $this->status,
             'msg' => $this->msg,
             'data' => $this->data,
-        ]);
+        ];
+        if ($this->more) {
+            $result['more'] = $this->more;
+        }
+
+        return json_encode($result);
     }
 
 }
